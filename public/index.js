923476da7d2bdf9c7ce6f2fc6e59c930d5a6483b
chrisphailey <93353718+chrisphailey@users.noreply.github.com>
2022-05-09 19:57:06 -0500

    img path

---
+++ public/index.js
@@ -0,0 +1,9 @@
+function turnOnLight() {
+  console.log("working");
+  document.getElementById("section-9").style.backgroundImage =
+    "url(./assets/images/glow-light.jpg";
+}
+
+// document
+//   .getElementById("section-9-btn")
+//   .addEventListener("click", turnOnLight());
